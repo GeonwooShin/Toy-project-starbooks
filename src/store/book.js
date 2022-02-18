38c5bf868f0f700990fdc27efd598b1c77a29ff2
GeonwooShin <rjsdnslawkd@naver.com>
2022-02-19 01:27:29 +0900

    커밋 에러 수정

--- src/store/book.js
+++ src/store/book.js
@@ -8,15 +8,7 @@ export default {
   state: () => ({
     books: [],
     totalpage: [],
-    theBook: {},
-    homeBooks1: {},
-    homeBooks2: {},
-    homeBooks3: {},
-    homeBooks4: {},
-    homeBooks5: {},
-    searchHomeBooksIdStar: [],
-    searchHomeBooksIdHot: []
-
+    theBook: {}
   }),
   getters: {},
   // state에 정의된 변수를 변경하도록 하는 역할, 동기처리로써 state에 정의된 변경사항을 추적할 수 있게한다.
@@ -60,100 +52,7 @@ export default {
           theBook: {}
         })
       }
-    },
-
-
-    //--------------!!!!!homeBooks Aggro Start!!!!!-----------------
-    async homeBooks1({ commit }, payload) {
-      try {
-        const res = await _fetchBooks(payload)
-        // const { content } = res.data
-        commit('updateState', {
-          homeBooks1: res.data
-        })
-      } catch(error) {
-        commit('updateState', {
-          homeBooks1: {}
-        })
-      }
-    },
-    async homeBooks2({ commit }, payload) {
-      try {
-        const res = await _fetchBooks(payload)
-        // const { content } = res.data
-        commit('updateState', {
-          homeBooks2: res.data
-        })
-      } catch(error) {
-        commit('updateState', {
-          homeBooks2: {}
-        })
-      }
-    },
-    async homeBooks3({ commit }, payload) {
-      try {
-        const res = await _fetchBooks(payload)
-        // const { content } = res.data
-        commit('updateState', {
-          homeBooks3: res.data
-        })
-      } catch(error) {
-        commit('updateState', {
-          homeBooks3: {}
-        })
-      }
-    },
-    async homeBooks4({ commit }, payload) {
-      try {
-        const res = await _fetchBooks(payload)
-        // const { content } = res.data
-        commit('updateState', {
-          homeBooks4: res.data
-        })
-      } catch(error) {
-        commit('updateState', {
-          homeBooks4: {}
-        })
-      }
-    },
-    async homeBooks5({ commit }, payload) {
-      try {
-        const res = await _fetchBooks(payload)
-        // const { content } = res.data
-        commit('updateState', {
-          homeBooks5: res.data
-        })
-      } catch(error) {
-        commit('updateState', {
-          homeBooks5: {}
-        })
-      }
-    },
-    async searchHomeBooksIdStar({ commit }, payload) {
-      console.log('123123')
-      const res = await _fetchBooks({
-        ...payload
-      })
-      console.log(res)
-      const { content, totalPages } = res.data
-      commit('updateState', {
-        searchHomeBooksIdStar: content,
-        totalpage: totalPages
-      })
-    },
-    async searchHomeBooksIdHot({ commit }, payload) {
-      console.log('123123')
-      const res = await _fetchBooks({
-        ...payload
-      })
-      console.log(res)
-      const { content, totalPages } = res.data
-      commit('updateState', {
-        searchHomeBooksIdHot: content,
-        totalpage: totalPages
-      })
     }
-    //--------------!!!!!!homeBooks Aggro End!!!!!!!-----------------
   }
 }
 
